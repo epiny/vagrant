81ba2e2079088039ad6aa4d526f6f49281b608ac
nybuddy <alfest@gmail.com>
2014-10-02 00:27:43 -0400

    added IMAP

--- install.sh
+++ install.sh
@@ -9,4 +9,9 @@ curl -sL https://deb.nodesource.com/setup | sudo bash -
 echo "--- Install Node"
 sudo apt-get install -y build-essential nodejs
 
+
+
+echo "--- Install IMAP client  ---"
+
+sudo npm install imap
 echo "--- All Done! ---"
