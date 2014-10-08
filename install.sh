488cb2163272ce96fa299c2a06481b3561f336c3
shimonenator <shimonenator@gmail.com>
2014-10-08 15:20:16 -0400

    Update provision file

--- install.sh
+++ install.sh
@@ -9,8 +9,17 @@ curl -sL https://deb.nodesource.com/setup | sudo bash -
 echo "--- Install Node"
 sudo apt-get install -y build-essential nodejs
 
-echo "--- Install Dependencies --- "
+echo "--- Install Node Dependencies --- "
 
-sudo npm install
+npm install -g gulp
+npm install -g bower
+
+echo "--- Install Project Dependencies --- "
+
+npm install /var/www
+
+echo "--- Install Bower Dependencies --- "
+
+cd /var/www && bower install --allow-root && cd -
 
 echo "--- All Done! ---"
